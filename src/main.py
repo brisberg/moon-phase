5c15d1951108a1f5302b41982a4cb8ca2381be68
brisberg <brandon.risberg@gmail.com>
2024-06-22 22:35:45 -0700

    Adjusts main script to print the next fullmoon and current moon phase

--- src/main.py
+++ src/main.py
@@ -1,6 +1,11 @@
 import ephem
+from datetime import datetime
+from moon.moon import get_phase_progress_on_day, get_phase_from_progression
 
 def mainfunction():
     print('Welcome to Moon Phase!')
 
-print(ephem.next_full_moon('2000'))
+date = datetime.now()
+
+print(get_phase_from_progression(get_phase_progress_on_day(date.year, date.month, date.day)))
+print(f'Next Full Moon: {ephem.next_full_moon(date)}')
